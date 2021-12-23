a42eba32ee5d5bdfc3d2439c68f4fca4e27e7896
mellow <stay.mellow.993@gmail.com>
2021-12-23 22:08:49 +0100

    convert method implemented

---
+++ Arbeitszeiterfassung/Client/Common/Converters/TimeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Arbeitszeiterfassung.Client.Common.Converters
+{
+    public class TimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var Datum = (DateTime)value;
+            return Datum.ToLongTimeString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
