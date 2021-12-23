462bb99a31f5cb64fe8b74d8fc5dc7fb9be3487e
mellow <stay.mellow.993@gmail.com>
2021-12-23 20:05:01 +0100

    base classes and interface implemented

--- Arbeitszeiterfassung/Client/Common/DelegateCommand.cs
+++ Arbeitszeiterfassung/Client/Common/DelegateCommand.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Arbeitszeiterfassung.Client.Common
 {
-    class DelegateCommand
+    class DelegateCommand : ICommand
     {
+        private Action _execute;
+        public DelegateCommand(Action execute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException(nameof(execute), "no execution");
+            _execute = execute;
+        }
+        public event EventHandler CanExecuteChanged;
+        public void OnExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        public void Execute(object parameter) => _execute.Invoke();
+        public bool CanExecute(object parameter) => true;
     }
 }
---
+++ Arbeitszeiterfassung/Client/ViewModel/TimekeepingViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Arbeitszeiterfassung.Client.Common;
+using Arbeitszeiterfassung.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Arbeitszeiterfassung.Client.ViewModel
+{
+    class TimekeepingViewModel : ViewModelBase
+    {
+
+        public TimekeepingViewModel()
+        {
+            _startTimekeepingCommand = new DelegateCommand(StartTimekeeping);
+        }
+
+        private DelegateCommand _startTimekeepingCommand;
+        public ICommand StartTimekeepingCommand { get => _startTimekeepingCommand; }
+
+
+
+        #region Methods
+        private void StartTimekeeping()
+        {
+
+        }
+        #endregion
+
+    }
+}
