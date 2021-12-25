7a3e0c59476af6719673846224b1d9a718d39406
mellow <stay.mellow.993@gmail.com>
2021-12-25 10:00:43 +0100

    bind ui to timekeeping view-model

--- Arbeitszeiterfassung/Client/ViewModel/TimekeepingViewModel.cs
+++ Arbeitszeiterfassung/Client/ViewModel/TimekeepingViewModel.cs
@@ -6,24 +6,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows;
+using Arbeitszeiterfassung.Model;
 
 namespace Arbeitszeiterfassung.Client.ViewModel
 {
     class TimekeepingViewModel : ViewModelBase
     {
 
+        private WorkTimeMeasurementModel _workTimeMeasurementModel;
+        public WorkTimeMeasurementModel WorkTimeMeasurementModel
+        {
+            get => _workTimeMeasurementModel;
+            set => _workTimeMeasurementModel = value;
+        }
         public TimekeepingViewModel()
         {
-            _startTimekeepingCommand = new DelegateCommand(StartTimekeeping);
+            //var _workTimeMeasurementModel = new WorkTimeMeasurementModel(); // why I can't initiate an object and use it in the entire class
+            PrepareCommands();
         }
 
+        #region Commands
         private DelegateCommand _startTimekeepingCommand;
+        private DelegateCommand _startBreakTimeCommand;
+        private DelegateCommand _finishWorkCommand;
+        private DelegateCommand _exitWindowCommand;
+        
         public ICommand StartTimekeepingCommand { get => _startTimekeepingCommand; }
+        public ICommand StartBreakTimeCommand { get => _startBreakTimeCommand; }
+        public ICommand FinishWorkCommand { get => _finishWorkCommand; }
+        public ICommand ExitWindowCommand { get => _exitWindowCommand; }
+        #endregion
+
+        public void PrepareCommands()
+        {
+            _startTimekeepingCommand = new DelegateCommand(StartTimekeeping);
+            _startBreakTimeCommand = new DelegateCommand(StartBreakTime);
+            _finishWorkCommand = new DelegateCommand(FinishWork);
+            _exitWindowCommand = new DelegateCommand(ExitWindow);
+        }
 
         #region Methods
         private void StartTimekeeping()
         {
+        }
+        
+        private void StartBreakTime()
+        {
+         
+            //MessageBox.Show(foo2.FinishWork.ToString());
+        }
 
+        private void FinishWork()
+        {
+            //WorkTimeMeasurementModel foo2 = new WorkTimeMeasurementModel();
+        }
+
+        private void ExitWindow()
+        {
+            Application.Current.Shutdown();
         }
         #endregion
 
