5588e43be87b31fbad64df0585c163a45f0ae798
mellow <stay.mellow.993@gmail.com>
2022-01-18 18:48:48 +0100

    Cleanup dispatch

--- Arbeitszeiterfassung/Model/Dispatch.cs
+++ Arbeitszeiterfassung/Model/Dispatch.cs
@@ -8,11 +8,9 @@ namespace Arbeitszeiterfassung.Model
     public class Dispatch : EventArgs
     {
         private readonly NotifyUser _notifyuser = new NotifyUser();
-
         private readonly string _info = "Hinweis";
         private readonly string _warning = "Warnung";
         private readonly string _error = "Fehler";
-
         private readonly string _startWork = "Arbeitsbeginn";
         private readonly string _startBreak = "Pause";
         private readonly string _continueWork = "Weiterarbeiten";
@@ -20,13 +18,11 @@ namespace Arbeitszeiterfassung.Model
         private readonly string _noSaveTime = "Arbeitszeit konnte nicht gespeichert werden";
         private readonly string _finishWork = "Arbeitsende";
         private readonly string _serviceTimes = "Beachten Sie die Servicezeiten";
-
-
         private readonly ToolTipIcon __info = ToolTipIcon.Info;
         private readonly ToolTipIcon __warning = ToolTipIcon.Warning;
         private readonly ToolTipIcon __error = ToolTipIcon.Error;
 
-        public Dispatch() {}
+
         public void StartWorking(object sender, Dispatch e) => _notifyuser.ShowInformation(_info, _startWork, __info);
         public void StartBreak(object sender, Dispatch e) => _notifyuser.ShowInformation(_info, _startBreak, __info);
         public void ContinueWork(object sender, Dispatch e) => _notifyuser.ShowInformation(_info, _continueWork, __info);
@@ -34,7 +30,6 @@ namespace Arbeitszeiterfassung.Model
         public void SaveTimes(object sender, Dispatch e) => _notifyuser.ShowInformation(_info, _saveTime, __info);
         public void NoSaveTimes(object sender, Dispatch e) => _notifyuser.ShowInformation(_error, _noSaveTime, __error);
         public void ServiceTimes(object sender, Dispatch e) => _notifyuser.ShowInformation(_warning, _serviceTimes, __warning);
-
     }
 
     public class NotifyUser : ObservableRecipient 
@@ -60,8 +55,5 @@ namespace Arbeitszeiterfassung.Model
 
         public void ShowInformation(string infotype, string info, ToolTipIcon mode)
             =>  _notifyIcon.ShowBalloonTip(_timeOfAppearance, infotype, info, mode);
-        
     }
-
-   
 }
